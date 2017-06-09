1b92dba963f933db240aba124678713408d49fdb
Erik Bates <erik.s.bates@gmail.com>
2017-06-08 20:02:07 -0500

    Fixed model class usage.

--- UserSignup/Controllers/UserController.cs
+++ UserSignup/Controllers/UserController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-//using UserSignup.Models;
+using UserSignup.Models;
 using UserSignup.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -13,9 +13,12 @@ namespace UserSignup.Controllers
     public class UserController : Controller
     {
         // GET: /<controller>/
-        public IActionResult Index(AddUserViewModel user)
+        public IActionResult Index(User user)
         {
-            if (user == null) user = new AddUserViewModel();
+            if (user == null)
+            {
+                user = new User();
+            }
             return View(user);
         }
 
@@ -30,19 +33,17 @@ namespace UserSignup.Controllers
         {
             if (ModelState.IsValid)
             {
-                AddUserViewModel user = new AddUserViewModel()
+                User user = new User
                 {
                     Username = addUserViewModel.Username,
                     Email = addUserViewModel.Email,
                     Password = addUserViewModel.Password,
                     Verify = addUserViewModel.Verify
                 };
-                return RedirectToAction("Index", user);
+                return RedirectToAction("Index", new User { Username = addUserViewModel.Username });
             }
 
             return View(addUserViewModel);
         }
     }
-
-
 }
