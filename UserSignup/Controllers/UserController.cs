51afeb281b45f8fd30afca0a25223df7fff169da
Erik Bates <erik.s.bates@gmail.com>
2017-06-08 19:25:29 -0500

    Added ViewModels and validation.

---
+++ UserSignup/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using UserSignup.Models;
+using UserSignup.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserSignup.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index(AddUserViewModel user)
+        {
+            if (user == null) user = new AddUserViewModel();
+            return View(user);
+        }
+
+        public IActionResult Add()
+        {
+            AddUserViewModel addUser = new AddUserViewModel();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddUserViewModel addUserViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                AddUserViewModel user = new AddUserViewModel()
+                {
+                    Username = addUserViewModel.Username,
+                    Email = addUserViewModel.Email,
+                    Password = addUserViewModel.Password,
+                    Verify = addUserViewModel.Verify
+                };
+                return RedirectToAction("Index", user);
+            }
+
+            return View(addUserViewModel);
+        }
+    }
+
+
+}
---
+++ UserSignup/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserSignup.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [EmailAddress(ErrorMessage = "Enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Verify Password")]
+        [Compare("Password", ErrorMessage = "Passwords must match")]
+        public string Verify { get; set; }
+    }
+}
